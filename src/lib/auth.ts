0cf0a9263fba8874779fc5db183c5ba600261fed
rifat <rifat999por@gmail.com>
2026-06-17 21:39:04 +0600

    impliment seed admin process

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,42 @@
-import { betterAuth } from "better-auth";
+import { betterAuth, string } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
 import { prisma } from "./prisma";
 
+export enum UserRole{
+  USER="USER",
+  PROVIDER="PROVIDER",
+  ADMIN="ADMIN"
+}
+
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
-    }),
-     emailAndPassword: { 
-    enabled: true, 
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "USER",
+        required: false,
+      },
+      phone: {
+        type: "string",
+        required: false,
+      },
+      status: {
+        type: "string",
+        defaultValue: "ACTIVE",
+        required: false,
+      },
+      address: {
+        type: "string",
+        required: false,
+      },
+    },
   },
 });
