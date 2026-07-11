65718d4e80421fa2d330b24dd9f0b1d49ee18904
rifat <rifat999por@gmail.com>
2026-07-11 16:19:51 +0600

    fixed better auth error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -11,14 +11,13 @@ export enum UserRole {
 
 console.log("BETTER_AUTH_URL:", process.env.BETTER_AUTH_URL);
 console.log("APP_URL:", process.env.APP_URL);
-console.log("DATABASE_URL exists:", !!process.env.DATABASE_URL);
-console.log("BETTER_AUTH_SECRET exists:", !!process.env.BETTER_AUTH_SECRET);
+
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
-  baseURL:"https://food-hub-client-iota.vercel.app",
+  baseURL:"https://food-hub-server-p1vh.onrender.com/api/auth",
   trustedOrigins: [
     "https://food-hub-client-iota.vercel.app",
     "http://localhost:3000",
